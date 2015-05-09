8c8a3bddaabb0f3a4acebae890b454a8a7b24e19
Roberto Guerra <uris77@gmail.com>
2015-05-09 11:41:26 -0600

    Resets state during page transitions

--- src/cljs/proto/core.cljs
+++ src/cljs/proto/core.cljs
@@ -7,7 +7,7 @@
             [cljsjs.react :as react]
             [proto.goods :as goods]
             [proto.shops :as shops]
-            [proto.barcode-picture :as picture]
+            [proto.state :as state]
             [proto.util :refer [validate-item]])
   (:import goog.History))
 
@@ -25,27 +25,34 @@
 (defn current-page []
   [:div [(session/get :current-page)]])
 
+(defn reset-state!
+  []
+  (state/clear-barcode!)
+  (state/reset-new-shop!)
+  (state/reset-new-good!))
+
 ;; -------------------------
 ;; Routes
 (secretary/set-config! :prefix "#")
 
 (secretary/defroute "/goods/create" []
-  ;;(picture/start)  
-  (session/put! :current-page #'goods/create-good-form)
-  )
+  (session/put! :current-page #'goods/create-good-form))
 
 (secretary/defroute "/about" []
   (session/put! :current-page #'about-page))
 
 (secretary/defroute "/" []
   (goods/fetch-goods 0)
+  (reset-state!)
   (session/put! :current-page #'goods/goods-list))
 
 (secretary/defroute "/goods" []
+  (reset-state!)
   (goods/fetch-goods 0)
   (session/put! :current-page #'goods/goods-list))
 
 (secretary/defroute "/shops" []
+  (reset-state!)
   (shops/fetch-shops 0)
   (session/put! :current-page #'shops/shops-list)
   
